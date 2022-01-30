619b515495febed9b2882b460a0537546d5d01ab
yatinsonar5 <yatinsonar59@gmail.com>
2022-01-30 17:45:44 +0530

    cors

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const path = require("path");
 const connectDB = require("./config/db");
 connectDB();
 
+//Cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  }
+  
+  // Default configuration looks like
+  // {
+  //     "origin": "*",
+  //     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //     "preflightContinue": false,
+  //     "optionsSuccessStatus": 204
+  //   }
+  
+  app.use(cors(corsOptions))
+
 app.use(express.static("public"));
 app.use(express.json());
 
