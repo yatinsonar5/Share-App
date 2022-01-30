711a4454e1757644b2bf8b44c75d56ca99a14903
yatinsonar5 <yatinsonar59@gmail.com>
2022-01-30 18:39:36 +0530

    Cors Undu

--- server.js
+++ server.js
@@ -2,26 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
-const cors = require('cors');
+// const cors = require('cors');
 
 const connectDB = require("./config/db");
 connectDB();
 
 //Cors
-const corsOptions = {
-    origin: process.env.ALLOWED_CLIENTS.split(',')
-    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-  }
+// const corsOptions = {
+//     origin: process.env.ALLOWED_CLIENTS.split(',')
+//     // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+//   }
   
-  // Default configuration looks like
-  // {
-  //     "origin": "*",
-  //     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  //     "preflightContinue": false,
-  //     "optionsSuccessStatus": 204
-  //   }
+//   // Default configuration looks like
+//   // {
+//   //     "origin": "*",
+//   //     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   //     "preflightContinue": false,
+//   //     "optionsSuccessStatus": 204
+//   //   }
   
-  app.use(cors(corsOptions))
+//   app.use(cors(corsOptions))
 
 app.use(express.static("public"));
 app.use(express.json());
