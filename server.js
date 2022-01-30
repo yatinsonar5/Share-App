d53b5b72b6391b7e27ec15507c978fda15f137d1
yatinsonar5 <yatinsonar59@gmail.com>
2022-01-30 18:35:10 +0530

    cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
+const cors = require('cors');
 
 const connectDB = require("./config/db");
 connectDB();
